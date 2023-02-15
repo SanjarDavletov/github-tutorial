dbe958fb3d415d80ddfc04b0d7f5d2930e222ff1
sanjar08 <sanjar675@gmail.com>
2023-02-16 00:13:46 +0500

    first commit

---
+++ 1-dars/main.js
@@ -0,0 +1,46 @@
+"use strict";
+
+// const ogri = {
+//   age: 30, // nuber
+//   sochi: "chorniy",// srting
+//   boyi: "uzunn",//string
+//   uchtakozi: false,//boolean
+// }
+
+// console.log(ogri.sochi);
+
+// const colors = ["red" , "green" , "blue" [""] , {}  ] //massiv, massiv ichida massiv ham ishlatsa boladi va obyekt ham
+// console.log(colors[0]);
+
+// alert("Hello World")
+
+// const place = ("Are you here") // confirm true yoki false qiymatlarni qaytaradi
+// console.log(place);
+
+// const hozir = +prompt("Nechchi yoshsan" , "18")
+// console.log(hozir+10);
+
+// const favourteColor = [];
+// favourteColor[0] = prompt("Whats your favorite color numer1", "")
+// favourteColor[1] = prompt("Whats your favorite color numer2", "")
+// favourteColor[2] = prompt("Whats your favorite color numer3", "")
+// console.log(favourteColor);
+
+// let incr = 10;
+// let decr = 10;
+
+// console.log(incr++);  // ikkita plus 1 qoshadi
+// console.log(decr--);  //ikkita minus 1ni ayirda
+
+// console.log(incr);
+// console.log(decr);
+
+// console.log(10 % 2);  // % qoldiqni oladi
+
+// console.log(5*5=="25");  //bu == bolsa qiymatlarini tekshiradi
+// console.log(5*5==="25");  //bu=== bolsa ham qiymat ham malumot turlarini tekshiradi
+
+// console.log(5 + "9");
+
+
+//  && bu va mano
